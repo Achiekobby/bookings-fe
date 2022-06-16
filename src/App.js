7c15f8d505218ea75b7840fa3cd74ac96b5984b8
Acheampong Nana <achiekobby17@gmail.com>
2022-06-16 17:03:46 +0000

    Project setup continued

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home/Home"
+import List from './Pages/List/List';
+import Hotel from './Pages/Hotel/Hotel';
+import "./Styles/Css/App.css"
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
     </div>
   );
 }
