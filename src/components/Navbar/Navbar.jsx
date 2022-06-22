9cc798399f0605fe9e92699b3e54cafc9a712e89
Acheampong Nana <achiekobby17@gmail.com>
2022-06-22 14:58:20 +0000

    Desktop Navbar implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import  "../../Styles/Css/Navbar.css"
+import { TbBrandBooking } from "react-icons/tb"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <div className="navContainer">
+            <div className="logo_item">
+                <span className="logo_icon"><TbBrandBooking/></span><span className="logo">kwaBooking</span>
+            </div>
+            <div className="navItems">
+                <button className="navButton">Register</button>
+                <button className="navButton">Login</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
